852e47d378f22663fe613a672aa415b9e0e9ff48
Tian23 <tian23@illinois.edu>
2022-10-31 21:14:18 -0500

    fix script error

--- script/update_code.sh
+++ script/update_code.sh
@@ -7,7 +7,7 @@ do
     echo VM$val Updated
 done
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp2-hangy6-tian23; git clone -b dev git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp3-hangy6-tian23; git clone -b dev git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
 echo VM10 Updated
 
 echo "-----------All VMs Have Been Updated!------------"
