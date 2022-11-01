c6aa9a5361755d729e441c1626c231a5760eb80a
Tian23 <tian23@illinois.edu>
2022-10-31 20:45:23 -0500

    add setup script

---
+++ script/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for val in {1..9}
+do
+    echo VM$val Updating
+    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd mp3-hangy6-tian23; pip install -r requirements.txt"
+    echo VM$val Updated
+done
+echo VM10 Updating
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp3-hangy6-tian23; pip install -r requirements.txt"
+echo VM10 Updated
+
+echo "-----------All VMs Have Been Setup------------"
