8afd78714034059ea049ff4846a5d68471526bdb
Tian23 <tian23@illinois.edu>
2022-10-31 21:42:06 -0500

    fix script error

--- script/pull.sh
+++ script/pull.sh
@@ -3,11 +3,11 @@
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "git pull"
+    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd mp3-hangy6-tian23; git pull"
     echo VM$val Updated
 done
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "git pull"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp3-hangy6-tian23; git pull"
 echo VM10 Updated
 
 echo "-----------All VMs Have Been Updated!------------"
