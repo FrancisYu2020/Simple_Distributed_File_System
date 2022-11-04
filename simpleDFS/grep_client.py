2e756fc17cc02c1681869e00c1e34dc6ce65abc8
Tian23 <tian23@illinois.edu>
2022-11-04 18:16:03 -0500

    add distribute grep part

---
+++ simpleDFS/grep_client.py
@@ -0,0 +1,86 @@
+import threading
+import socket
+import sys
+import time
+
+PORT = 20001
+
+def get_host(number):
+    '''
+    param number: the illinois vm number,ranges from 01 to 10
+    return: host string
+    '''
+    return "fa22-cs425-22%02d.cs.illinois.edu" % number
+    
+class Client:
+    def __init__(self,args):
+        self.args = args
+        # a map to record the search time cost in each file. key: file name, value: time cost
+        self.time_cost = {}
+        self.total_lines = 0
+        
+    def client_program(self, command, host, port):
+        '''
+        get the data from server and print the required information
+        param command: the gerp command
+        param host: host name
+        param port: port number
+        return: None
+        '''
+        try:
+            time_start = time.time()
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((host,port))
+                s.sendall(command.encode('utf-8'))
+                data = b''
+                deadline = time.time() + 5
+                while True:
+                    bytes_received = s.recv(4096)
+                    data += bytes_received
+                    if bytes_received:
+                        s.send(b'FINISH')
+                        break
+                    if time.time() > deadline:
+                        print("[ERROR]", "connection timeout exceeded 5 seconds")
+                        return                
+            time_end = time.time()
+            time_cost = time_end - time_start
+
+            received_data = data.decode('utf-8').strip("\n")
+            filename, line_cnt = received_data.split(":")
+            self.time_cost[filename] = time_cost
+            self.total_lines += int(line_cnt)
+            print("[RESULT]", "host is %s, " % host, end = "")
+            print("{filename} has {line_count} matched lines, ".format(filename = filename, line_count = line_cnt), end = "")
+            print("time cost is %f seconds" % time_cost, end = "\n")
+        except:
+            raise Exception("error connect to server")
+
+    def query(self):
+        '''
+        send the query request and get data in 10 virtual machines
+        return: None
+        '''
+        time_start = time.time()
+        threads = []
+        # change to 11 for grep all
+        for i in range(1,6):
+            host = get_host(i)
+            t = threading.Thread(target = self.client_program, args = (self.args, host, PORT))
+            threads.append(t)
+            t.start()
+        
+        for t in threads:
+            t.join()
+
+        time_end = time.time()
+        self.time_cost["total"] = time_end - time_start
+        print("Total lines are %d" % self.total_lines)
+
+if __name__=="__main__":
+    if len(sys.argv) < 3:
+        print("Error: Input arguments are not sufficient")
+    else:
+        s = ' '.join(sys.argv[1:])
+        c = Client(s)
+        c.query()
---
+++ simpleDFS/grep_server.py
@@ -0,0 +1,73 @@
+import socket
+import re
+import os
+
+HOST = socket.gethostname()
+PORT = 20001
+
+
+def grep(request):
+    """
+    executes `grep -c pattern`/ `grep -Ec pattern` on all *.log files in the same directory
+    param request: the grep request from user
+    return: line count message or error message
+    """
+    args = request.split()
+    if len(args) != 3 or args[0] != 'grep' or (args[1] != '-c' and args[1] != '-Ec'):
+        return "Wrong format! Correct format: `grep -c pattern` or `grep -Ec pattern`"
+    res = ''
+    pattern = args[2]
+    if pattern[0] == '\"' and pattern[-1] == '\"':
+        pattern = pattern[1:-1]
+    print("updated pattern is: ", pattern)
+    # put all files under current directory in a list
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    # filter to all *.log files
+    log_files = [f for f in files if f.endswith('.log')]
+    # iterate all log files
+    for file in log_files:
+        with open(file, encoding="utf-8") as f:
+            c_cnt = ec_cnt = 0  # c_cnt is for exact pattern matching / ec_cnt is for regex pattern search
+            for line in f:
+                if line.count(pattern) > 0:
+                    c_cnt += 1
+                if len(re.findall(pattern, line)) > 0:
+                    ec_cnt += 1
+            if args[1] == '-c':
+                res += "{file}:{line_count}\n".format(file=file, line_count=c_cnt)
+            elif args[1] == '-Ec':
+                res += "{file}:{line_count}\n".format(file=file, line_count=ec_cnt)
+            else:
+                return "Only grep -c / -Ec options are supported!"
+    return res
+
+
+def server_program():
+    """
+    server program that listens on (HOST, PORT)
+    takes in the request, executes grep(), and return grep()'s result to requester
+    return: None
+    """
+    print("[INFO]: MP1 server started")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+
+    while True:
+        conn, addr = s.accept()
+        print("[INFO]: connection from: " + str(addr))
+        while True:
+            request = conn.recv(4096).decode("utf-8")
+            if request:
+                if request == 'FINISH':
+                    print("[INFO]: connection from: " + str(addr) + "ended!")
+                    break
+                print("[INFO] client request: " + str(request))
+                result = grep(request)
+                print("[INFO]: result is: ", result)
+                conn.send(result.encode("utf-8"))
+    conn.close()
+
+
+if __name__ == '__main__':
+    server_program()
