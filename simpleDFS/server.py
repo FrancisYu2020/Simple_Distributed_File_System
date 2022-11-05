4768c119ba936246e3198fcb392404e63a3e13b8
Tian23 <tian23@illinois.edu>
2022-11-04 21:36:56 -0500

    test

--- simpleDFS/server.py
+++ simpleDFS/server.py
@@ -2,6 +2,7 @@ import name_node
 import data_node
 import fd
 from multiprocessing import Process
+import threading
 
 
 class Server:
@@ -11,12 +12,12 @@ class Server:
         self.dn = data_node.DataNode()
 
     def run(self):
-        p = Process(target=self.fd.run)
-        p1 = Process(target=self.nn.run)
-        p2 = Process(target=self.dn.run_data_node)
-        p.start()
-        p1.start()
-        p2.start()
+        t0 = threading.Thread(target=self.fd.run)
+        t1 = threading.Thread(target=self.nn.run_name_node)
+        t2 = threading.Thread(target=self.dn.run_data_node)
+        t0.start()
+        t1.start()
+        t2.start()
     
 
 if __name__ == '__main__':
