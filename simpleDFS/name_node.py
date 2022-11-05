6681b7b70d1dc87fba7bd28bf16715935c634744
Tian23 <tian23@illinois.edu>
2022-11-05 18:51:17 -0500

    fix typo

--- simpleDFS/name_node.py
+++ simpleDFS/name_node.py
@@ -10,7 +10,7 @@ import failure_detector
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
-                    datefmt='%Y-%m-%d,%H:%M:%S'
+                    datefmt='%Y-%m-%d,%H:%M:%S',
                     filename='Namenode.log',
                     filemode='w')
 
