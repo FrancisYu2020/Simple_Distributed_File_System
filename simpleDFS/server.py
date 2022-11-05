024f813f08231ebe930a6f0e4e697bac0b4700f3
Tian23 <tian23@illinois.edu>
2022-11-04 21:53:40 -0500

    test

--- simpleDFS/server.py
+++ simpleDFS/server.py
@@ -12,16 +12,16 @@ t0 = threading.Thread(target = name_node.run, args=[failure_detector])
 # data node server thread
 t1 = threading.Thread(target = data_node.run_data_node)
 # failure detector
-t2 = threading.Thread(target = failure_detector.run)
+# t2 = threading.Thread(target = failure_detector.run)
     
 t0.start()
 print("t0 start")
 t1.start()
 print("t1 start")
-t2.start()
-print("t2 start")
+# t2.start()
+# print("t2 start")
     
 t0.join()
 t1.join()
-t2.join()
+# t2.join()
 
