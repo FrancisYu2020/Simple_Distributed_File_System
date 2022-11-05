09b9bdc2b02fa84fc8cc14039e46acb2f0bf8aef
Tian23 <tian23@illinois.edu>
2022-11-04 22:02:15 -0500

    test

--- simpleDFS/name_node.py
+++ simpleDFS/name_node.py
@@ -6,7 +6,7 @@ import threading
 import socket
 import time
 import logging
-import simpleDFS.failure_detector as failure_detector
+import failure_detector
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
--- simpleDFS/server.py
+++ simpleDFS/server.py
@@ -1,27 +1,27 @@
 import name_node
 import data_node
-import simpleDFS.failure_detector as failure_detector
+import failure_detector
 from multiprocessing import Process
 import threading
     
 
 
 MASTER_HOST = "fa22-cs425-2205.cs.illinois.edu"
-fd = None
+fd = failure_detector.Server(MASTER_HOST)
 t0 = threading.Thread(target = name_node.run, args=[failure_detector])
 # data node server thread
 t1 = threading.Thread(target = data_node.run_data_node)
 # failure detector
-# t2 = threading.Thread(target = failure_detector.run)
+t2 = threading.Thread(target = fd.run)
     
 t0.start()
 print("t0 start")
 t1.start()
 print("t1 start")
-# t2.start()
-# print("t2 start")
+t2.start()
+print("t2 start")
     
 t0.join()
 t1.join()
-# t2.join()
+t2.join()
 
