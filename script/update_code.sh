e8c399d82bd0a17299d2710d962590b981334b5a
Tian23 <tian23@illinois.edu>
2022-11-06 12:13:03 -0600

    change script

--- script/update_code.sh
+++ script/update_code.sh
@@ -3,11 +3,11 @@
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "rm -rf mp3-hangy6-tian23; git clone -b dev git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
+    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "rm -rf mp3-hangy6-tian23; git clone -b dev-backup git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
     echo VM$val Updated
 done
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp3-hangy6-tian23; git clone -b dev git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp3-hangy6-tian23; git clone -b dev-backup git@gitlab.engr.illinois.edu:hangy6/mp3-hangy6-tian23.git"
 echo VM10 Updated
 
 echo "-----------All VMs Have Been Updated!------------"
